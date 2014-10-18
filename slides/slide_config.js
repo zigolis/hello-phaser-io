d7465f739459001d36454f778ba3d212c72feb84
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2014-10-18 11:15:19 -0300

    Added slides

---
+++ slides/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Hello phaser.io<br>',
+    subtitle: 'Games com Javascript',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: '',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: '<a target="_blank" href="http://twitter.com/zigolis">Gabriel Zigolis / @zigolis</a>',
+    company: '',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@zigolis',
+    www: 'http://www.zigolis.com',
+    github: 'http://github.com/zigolis'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
